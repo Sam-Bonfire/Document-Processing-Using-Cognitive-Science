4888d222349fb260077ef4bf91720ae1f77a8345
Sam-Bonfire <pprathamesh98@gmail.com>
2020-01-26 21:46:58 +0530

    Created a basic Upload form

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
-@app.route('/')
+
+@app.route('/', methods=["GET"])
 def home():
-    return "Hello"
+    return '<html><body><form action = "/uploader" method = "POST" enctype = "multipart/form-data"><input type = "file" name = "file"/><input type = "submit" value = "Submit"/></form></body></html>'
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save('uploads/upload.png')
+        return '<h1>File Uploaded Successfully!!!</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
